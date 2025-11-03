0d1e8593b04701349640e3fabb2e4848fc2227fb
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-03 10:32:59 +0100

    subida

--- PedroDiaz/src/main/java/com/salesianostriana/dam/realbetis/Jugador/Model/Futbolista.java
+++ PedroDiaz/src/main/java/com/salesianostriana/dam/realbetis/Jugador/Model/Futbolista.java
@@ -30,7 +30,7 @@ public abstract class Futbolista {
     private String nacionalidad;
     private int numCamiseta;
     private double salarioMensualBase;
-                                                                    //Esto para la herencia, elimar.
+                                                                 //Esto para la herencia, elimar.
     @OneToOne(mappedBy = "futbolista", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Estadisticas estadisticas;
     @ManyToOne
